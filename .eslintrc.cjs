9913233f78ab1a87d985d86a5516a02c0604f2c5
David Jimenez <danimax.com@gmail.com>
2023-07-03 19:00:30 -0500

    Initial commit base on https://github.com/Danimaxpd/pdf-wrapper-template-the-lob-client

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['airbnb','airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/**
+ * Implement this interface. 
+ * Engine name is just an example, feel free to structure this as you see fit
+ */ 
+interface PDFWrapper {
+  engineName: string;
+}
+
+/**
+ * 
+ * @param {string} pathToPDFFile - The path to a pdf file
+ * @param {number} fileInfo - A binary represenation of the attributes required to render the PDF.
+ * You can choose which LSB represents which characteristic (annotation, encryption, search)
+ * @returns {PDFWrapper} - A PDFWrapper that wraps a selected PDF rendering engine
+ */
+export function renderPDF(pathToPDFFile: string, fileInfo: number): PDFWrapper {
+  // Implementation goes here. You can leave it in this file or move it elsewhere
+
+  const pdfWrapper: PDFWrapper = {
+    engineName: 'SoSoPDFEngine'
+  };
+  console.log({pdfWrapper});
+  return pdfWrapper;
+}
+
+// Feel free to use or remove this run function
+function run() {
+  const fileInfoByte = 0b111;
+  const pathToPDFFile = './example.pdf';
+  renderPDF(pathToPDFFile, fileInfoByte);
+}
+
+run();
